4a3969f776b696c7c6c3f5a36ad3230992d3ed1c
AhmetFurkanUtsal <furkan.utsal@hacettepe.edu.tr>
2023-07-21 14:53:32 +0300

    vertical

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,10 @@ public class PlayerController : MonoBehaviour
 {
 
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public float zRange = 10f;
 
     public GameObject projectilePrefab;
     // Start is called before the first frame update
@@ -31,7 +33,22 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+       
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x , transform.position.y, -zRange); 
+        }
+
+        if (transform.position.z > zRange) 
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
 
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
